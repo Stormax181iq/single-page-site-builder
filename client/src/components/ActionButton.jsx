8a4cabb111d9ce2d4734cd6aa3e01d5103840d28
stormax181iq <kelianchalier@protonmail.com>
2025-05-29 11:04:57 +0200

    fix Tailwind not generating utility classes

--- client/src/components/ActionButton.jsx
+++ client/src/components/ActionButton.jsx
@@ -1,7 +1,13 @@
-export default function ActionButton({ style = 1, children, className, ...props }) {
+export default function ActionButton({
+  style = 1,
+  children,
+  className,
+  ...props
+}) {
   return (
     <button
-      className={`bg-accent-${style} font-medium p-2 rounded-3xl text-black mx-1 cursor-pointer ${className}`}
+      style={{ backgroundColor: `var(--accent-${style})` }}
+      className={`font-medium p-2 rounded-3xl text-black mx-1 cursor-pointer ${className}`}
       {...props}
     >
       {children}
