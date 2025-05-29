c1038f7f2b84462be4f4ad5b87d82250bf927673
Kélian Chalier <kelianchalier@protonmail.com>
2025-05-29 10:43:38 +0200

    add interactivity on template card + font awesome icons

--- client/src/components/ActionButton.jsx
+++ client/src/components/ActionButton.jsx
@@ -1,7 +1,7 @@
 export default function ActionButton({ style = 1, children, className, ...props }) {
   return (
     <button
-      className={`bg-accent-${style} font-medium p-2 rounded-3xl text-black mx-1 ${className}`}
+      className={`bg-accent-${style} font-medium p-2 rounded-3xl text-black mx-1 cursor-pointer ${className}`}
       {...props}
     >
       {children}
--- client/src/components/TemplateCard.jsx
+++ client/src/components/TemplateCard.jsx
@@ -1,11 +1,18 @@
+import {useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ActionButton from "./ActionButton";
-
 export default function TemplateCard({}) {
+    const [selected, setSelected] = useState(false);
     return <div className="group relative w-64 rounded-xl">
         <img className="rounded-xl z-0" src="https://picsum.photos/1400/1200"/>
-        <div className="rounded-xl absolute flex justify-center items-center p-1 top-0 z-1 w-full h-full">
+        <div className="rounded-xl absolute flex justify-center items-center p-1 tp-0 z-1 w-full h-full">
+            {selected && (
+                <FontAwesomeIcon icon="fa-solid fa-check" />
+            )}
+        </div>
+            <div className="absolute border-2 border-black top-1 left-1 w-6 h-6 rounded-full bg-main-1"></div>
             <div className="absolute rounded-xl top-0 w-full h-full group-hover:bg-black opacity-25"></div>
-            <ActionButton className="z-2 hidden group-hover:block w-32">SELECT</ActionButton>
+            <ActionButton onClick={() => setSelected(!selected)} className="z-2 hidden group-hover:block w-32">SELECT</ActionButton>
         </div>
     </div>
 }
