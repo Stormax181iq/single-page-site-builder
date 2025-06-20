652f4c458f9538a23f197da0c59931bcd4a96377
stormax181iq <kelianchalier@protonmail.com>
2025-06-20 10:46:52 +0200

    add proper error handling for iframe when not authenticated

--- client/src/components/TemplateEditor.jsx
+++ client/src/components/TemplateEditor.jsx
@@ -1,4 +1,9 @@
+import useAuth from "../hooks/useAuth";
+import { Link } from "react-router";
+
+import ActionButton from "./ActionButton";
 export default function TemplateEditor() {
+  const { isAuthenticated } = useAuth();
   return (
     <div className="mx-[15vh] mt-8 mb-4">
       <h2 className="font-heading text-4xl font-semibold text-main-2 mb-2">
@@ -9,12 +14,27 @@ export default function TemplateEditor() {
           <p>test 1</p>
           <p>test 2</p>
         </div>
-        <div className="w-full h-full col-span-3">
-          <iframe
-            className="w-full h-full border rounded-lg"
-            src="/api/templates/files/car-rent/index.html"
-            frameborder="0"
-          ></iframe>
+        <div className="w-full col-span-3 border rounded-lg">
+          {isAuthenticated ? (
+            <iframe
+              className="w-full h-full"
+              src="/api/templates/files/car-rent/index.html"
+              frameborder="0"
+              allowFullScreen
+            ></iframe>
+          ) : (
+            <>
+              <p className="m-2 text-center">
+                Not authenticated. Your session may have expired. Please login
+                again :
+              </p>
+              <div className="w-full flex justify-center">
+                <ActionButton className="px-4">
+                  <Link to="/auth/login">Login</Link>
+                </ActionButton>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
